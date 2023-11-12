4bf95a1031bce0c524b9be2492c9adecdfa0b8f3
unknown <viet.nguyen3@monstar-lab.com>
2023-11-12 23:02:29 +0700

    Login sreen UI

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,6 +1,5 @@
 import { memo } from "react";
 
-import { Card } from "antd";
 import cx from "classnames";
 
 import styles from "./AuthLayout.module.scss";
@@ -11,8 +10,12 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className={cx(styles.container, "flex-center")}>
-      <Card>{children}</Card>
+    <div className={cx(styles.container, "my-6 px-8")}>
+      <div className="flex-space-between">
+        <p className="logo-text">backlog</p>
+        <p className="font-18">Contact us</p>
+      </div>
+      <div className="auth-content flex-center">{children}</div>
     </div>
   );
 };
